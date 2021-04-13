1d4c3112064ecb1ec6d1d4959bce894651660b4e
brian <riceb53@gmail.com>
2021-04-12 22:21:38 -0400

    one large commit with everything

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
+
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <p><input type="text" v-model="search"></p>
+    <div v-for="school in filterBy(schools, search, 'name')">
+      <p>name: {{school.name}}</p>
+      <p>dbn: {{school.dbn}}</p>      
+      <p>number_of_participants: {{school.number_of_participants}}</p>
+      <p>reading: {{school.reading}}</p>
+      <p>math: {{school.math}}</p>
+      <p>difference: {{school.math - school.reading}}</p>
+      <p>writing: {{school.writing}}</p>
+      <p>total: {{school.total}}</p>
+      <p>rank: {{school.rank}}</p>
+      <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import Vue2Filters from "vue2-filters";
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  mixins: [Vue2Filters.mixin],
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      search: "",
+      schools: []
+    };
+  },
+  created: function() {
+    axios.get('/api/schools').then(response => {
+      this.schools = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
+
